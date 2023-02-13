c3a79e43865e70a1dc573d04079e9fff0f97717f
Natalia <taliyared@gmail.com>
2023-02-13 13:37:19 +0200

    title attr added in task#2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,7 +9,7 @@ const createGalleryMarkup = galleryItems.map(({ preview, original, description }
   return  `
   <li class='gallery__item'>
     <a class='gallery__link' href="${original}">
-    <img class=gallery__image src="${preview}" alt="${description}"/></a>
+    <img class=gallery__image src="${preview}" alt="${description}" title="${description}"/></a>
   </li>`
 }).join('')
 galleryList.insertAdjacentHTML('afterbegin', createGalleryMarkup)

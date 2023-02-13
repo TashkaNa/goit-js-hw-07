00b8587458c2fe90c9cf915cc1a6bdd1f752c2e0
Natalia <taliyared@gmail.com>
2023-02-13 12:40:34 +0200

    deleted JSON files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,35 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 
 // Change code below this line
+console.log(galleryItems);
 
-const list = document.querySelector(".gallery")
-const listItem = galleryItems.map(({preview, original, description}) => { 
+const galleryList = document.querySelector(".gallery")
+galleryList.addEventListener('click', onImageClick)
+
+const createGalleryMarkup = galleryItems.map(({ preview, original, description }) => {
   return `
   <div class='gallery__item'>
   <a class='gallery__link' href="${original}">
   <img class=gallery__image src="${preview}" data-source="${original}" alt="${description}"/></a>
   </div>`
 }).join('')
-
-list.insertAdjacentHTML('afterbegin', listItem)
-console.log(galleryItems);
-
-list.addEventListener('click', onImageClick)
+galleryList.insertAdjacentHTML('afterbegin', createGalleryMarkup)
 
 function onImageClick(e) {
   e.preventDefault();
+  galleryList.addEventListener('keydown', onEscClick);
+  galleryList.addEventListener('click', onInstanceClose)
   if (e.target.nodeName !== 'IMG') {
-    return
+    return;
   }
   const instance = basicLightbox.create(`
     <img src="${e.target.dataset.source}" width="800" height="600">
-`)
-  instance.show()
+  `);
+  instance.show();
 
-  list.addEventListener('keydown', (e) => {
+  function onInstanceClose() {
+    instance.close()
+    galleryList.removeEventListener('keydown', onEscClick);
+  }
+
+  function onEscClick(e) {
     if (e.code === 'Escape') {
       instance.close()
+      galleryList.removeEventListener('click', onInstanceClose);
     }
-  });
-
+  }
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery")
+galleryList.addEventListener('click', onGalleryImageClick)
+
+const createGalleryMarkup = galleryItems.map(({ preview, original, description }) => {
+  return  `
+  <li class='gallery__item'>
+  <a class='gallery__link' href="${original}">
+  <img class=gallery__image src="${preview}" alt="${description}" title="${description}"/></a>
+  </li>`
+}).join('')
+galleryList.insertAdjacentHTML('afterbegin', createGalleryMarkup)
+
+
+
+function onGalleryImageClick(e) {
+  e.preventDefault();
+
+    if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+ 
+  const lightbox = new SimpleLightbox('.gallery__item .gallery__link', {
+    captionDelay: 250,
+    captionData: 'alt',
+  });
+  
+}
